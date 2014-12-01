3b2f5008745ce96ca0781a2540fd2d142ff130c5
liangchuan <cc14514@icloud.com>
2014-12-01 15:02:49 +0800

    修正

--- tools/session_counter/config.py
+++ tools/session_counter/config.py
@@ -1,12 +1,18 @@
+import logging
+
+logging_cfg = {
+	'file':'/home/appusr/var/log/session_counter.log',
+	'level':logging.DEBUG
+}
 
 dbconfig = {
-				'host':'192.168.2.13',
-				'schema':'db_emsg',
-				'user':'root',
-				'password':'123456'
-			}
+	'host':'192.168.2.13',
+	'schema':'db_emsg',
+	'user':'root',
+	'password':'123456'
+}
 				
 httpconfig = {
-				'url': 'http://182.254.210.135:4280'
-			}
+	'url': 'http://182.254.210.135:4280'
+}
 
--- tools/session_counter/get_total_count.py
+++ tools/session_counter/get_total_count.py
@@ -1,49 +1,61 @@
 #!/usr/bin/env python
-#coding=utf-8
+# coding=utf-8
 
 import urllib2, urllib
 import json
 import datetime
 import uuid
-import config
+import config,logging
 from tools import *
 
-now = datetime.datetime.now()
-time = now.strftime('%Y-%m-%d %H:%M:%S')
-hour = now.strftime('%Y-%m-%d-%H')
-date = now.strftime('%Y-%m-%d')
+logging.basicConfig(
+	filename=config.logging_cfg.get('file'),
+	level=config.logging_cfg.get('level')
+)
 
-body='{"sn":"' + uuid.uuid1().hex + '","service":"emsg_status","method":"get_total_count","params":{}}'
+if __name__ == '__main__':
+	import time
+	while True:
+		loop()
+		time.sleep(60)
 
-data = {'body' : body}
-f = urllib2.urlopen( url=config.httpconfig['url'], data=urllib.urlencode(data))
-response = f.read()
-
-res = json.loads(response)
-
-print json.dumps(res, ensure_ascii=False, indent=4) 
- 
-entity = res["entity"]["result"]
-
-for data in entity:
-		sample = EmsgStatSessionSample.create(
-						id=uuid.uuid1(),
-						domain=data["domain"],
-						time=time,
-						total_count=data["total_count"])
-
-		hourly = EmsgStatSessionHourly(
-						id=uuid.uuid1(),
-                        domain=data["domain"],
-                        time=hour,
-                        total_count=data["total_count"])
-
-		daily = EmsgStatSessionDaily(
-						id=uuid.uuid1(), 
-						domain=data["domain"],
-						time=date,
-						total_count=data["total_count"]) 
-
-		insertOrUpdateHourly(hourly)
-		insertOrUpdateDaily(daily)
+def loop():
+	now = datetime.datetime.now()
+	time = now.strftime('%Y-%m-%d %H:%M:%S')
+	hour = now.strftime('%Y-%m-%d-%H')
+	date = now.strftime('%Y-%m-%d')
+	
+	body = '{"sn":"' + uuid.uuid1().hex + '","service":"emsg_status","method":"get_total_count","params":{}}'
+	
+	data = {'body' : body}
+	f = urllib2.urlopen(url=config.httpconfig['url'], data=urllib.urlencode(data))
+	response = f.read()
+	
+	res = json.loads(response)
+	
+	logging.debug(json.dumps(res))
+	 
+	entity = res["entity"]["result"]
+	
+	for data in entity:
+			sample = EmsgStatSessionSample.create(
+							id=uuid.uuid1(),
+							domain=data["domain"],
+							time=time,
+							total_count=data["total_count"])
+	
+			hourly = EmsgStatSessionHourly(
+							id=uuid.uuid1(),
+	                        domain=data["domain"],
+	                        time=hour,
+	                        total_count=data["total_count"])
+	
+			daily = EmsgStatSessionDaily(
+							id=uuid.uuid1(),
+							domain=data["domain"],
+							time=date,
+							total_count=data["total_count"]) 
+	
+			insertOrUpdateHourly(hourly)
+			insertOrUpdateDaily(daily)
 

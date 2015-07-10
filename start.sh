d95fbc19374401507517982bb905210a77900da0
liangchuan <cc14514@icloud.com>
2015-07-10 14:16:27 +0800

    fix

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@ ps -ef |grep emsgadmin|awk '{print $2}'|xargs kill -9
 python manage.py collectstatic
 rm -rf */*.pyc
 rm -rf */*/*.pyc
-uwsgi --http :8800 --socket :8880 --chdir `pwd` --module django_wsgi
+uwsgi --http :8800 --socket :8880 --chdir `pwd` --module django_wsgi &

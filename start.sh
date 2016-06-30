bc0e2d2181bcc5fc08ec58a96e1a96d01961a46a
cc14514 <lance_fox0451@163.com>
2016-06-30 18:17:59 +0800

    fix

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
+#!/usr/bin/env bash
 ps -ef |grep emsgadmin|awk '{print $2}'|xargs kill -9
 python manage.py collectstatic
 rm -rf */*.pyc
 rm -rf */*/*.pyc
-uwsgi --http :8800 --socket :8880 --chdir `pwd` --module django_wsgi &
+uwsgi --http :8800 --socket :8880 --chdir `pwd` --module emsgadmin.wsgi &

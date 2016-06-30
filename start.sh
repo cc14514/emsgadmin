71588505b6c40791eb3118def7cf76a94f8360ba
cc14514 <lance_fox0451@163.com>
2016-06-30 18:38:27 +0800

    fix

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 ps -ef |grep emsgadmin|awk '{print $2}'|xargs kill -9
+ps -ef |grep get_total_count|awk '{print $2}'|xargs kill -9
 python manage.py collectstatic
 rm -rf */*.pyc
 rm -rf */*/*.pyc
 uwsgi --http :8800 --socket :8880 --chdir `pwd` --module emsgadmin.wsgi &
+cd tools/session_counter
+python get_total_count.py &
